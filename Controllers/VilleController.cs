0e8bd378e0ba7259de18655252dc9076219732d5
HalimBens <80537376+HalimBens@users.noreply.github.com>
2024-04-12 14:16:54 -0400

    version2

---
+++ Controllers/VilleController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using testDataGrid.Context;
+
+namespace testDataGrid.Controllers
+{
+    public class VilleController : Controller
+    {
+        private readonly TestDataGridDbContext _dbContext;
+
+        public VilleController(TestDataGridDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        [Route("/Ville/{idPays}")]
+        public IActionResult ListeVilles(int idPays)
+        {
+            return View();
+        }
+    }
+}
